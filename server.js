03542102b9343864162b6945dee39dff9456b5d1
Boanerges-Souza\Trinity <boanerges.souza@trinitysolucoes.com>
2019-10-31 09:49:12 -0300

    Preparando para enviar versão para heroku

--- server.js
+++ server.js
@@ -3,14 +3,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/anamnese'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req,res) => {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/anamnese/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4203);
